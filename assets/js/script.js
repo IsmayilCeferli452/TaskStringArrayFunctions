b6f8f59b630d0837fed39d4cd63e161367d1ec27
Ismayil Ceferli <ismayilej@code.edu.az>
2024-04-26 19:40:00 +0400

    Finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,54 @@
+"use strict";
 
-////1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
-// let n = 21;
 
-// function divide() {
-//     if(n%3==0 && n%7==0){
-//         console.log("Bolunur")
+//1) Verilmiş n - ədədinin 3-ə və 7-ə bölünüb-bölünməməsini tapan bir function yazin.
+
+// function divide(number) {
+//     if(number%3 == 0 && number%7 == 0){
+//         console.log("Bolunur");
 //     }else{
-//         console.log("Bolunmur")
+//         console.log("Bolunmur");
 //     }
 // }
 
+// let n = 21;
 // divide(n);
-
 
 
 ////2) Functiona musbet bir eded gelir, hemin funksiya gelen ededin faktorialini hesablayib geri qaytarmalidir. 
 
-
-// let n = 5;
-
-// function factorial(n){
-//     let sum = 1;
-//     for (let i = 1; i <= n; i++) {
-//         sum *= i
+// function factorial(number){
+//     let multiple = 1;
+//     for (let i = 1; i <= number; i++) {
+//         multiple *= i;
 //     }
-//     return sum
+//     return multiple;
 // }
 
+// let n = 4;
 // console.log(factorial(n));
 
 
 ////3) Her hansisa number elementleri olan arayin icindeki cut ededlerin kvadratlarinin cemini hesablayan bir function yazin.
 
-
 // let array = [1,2,3,4,5,6]
 
-// function sumofelements(array){
+// function sumOfOddNumbers(array){
 //     let sum = 0;
 //     let square = 0;
 //     for (let i = 0; i < array.length; i++) {
 //         if(array[i]%2 == 0){
-//             sum += array[i]
+//             sum += array[i];
 //         }
 //     }
 //     square = sum*sum;
 //     return square;
 // }
 
-// console.log(sumofelements(array));
+// console.log(sumOfOddNumbers(array));
 
 
 ////4) Funtiona-a  mail ve password gelir. Mail beraberdirse cavid@code.edu.az -a ve password beraberdirse 12345 -e, ekranda "Girish olundu" yazilsin, eks halda "Mail ve yaxud password sehvdir" yazilsin. 
-
-// let email = "cavid@code.edu.az";
-
-// let pass = "12345";
 
 // function login(mail, password){
 //     if(mail == "cavid@code.edu.az" && password == "12345"){
@@ -65,7 +59,8 @@
 //     }    
 // }
 
-
+// let email = "cavid@code.edu.az";
+// let pass = "12345";
 // login(email, pass);
 
 
@@ -73,7 +68,7 @@
 
 // let array = [1,2,3,4,5,6];
 
-// function sumofodd(array){
+// function sumOfOddNumbers(array){
 //     let sum = 0;
 //     for (let i = 0; i < array.length; i++) {
 //         if(i%2 == 1){
@@ -83,7 +78,7 @@
 //     return sum;
 // }
 
-// console.log(sumofodd(array));
+// console.log(sumOfOddNumbers(array));
 
 
 //// 6) Functiona ededlerden ibaret array gelir, function hemin arraydaki cut ededlerin sayini qaytarmalidir.
@@ -97,7 +92,7 @@
 //             count++;
 //         }
 //     }
-//     return count
+//     return count;
 // }
 
 // console.log(sumofodd(array));
@@ -128,18 +123,12 @@
 
 // let array = [student1, student2, student3];
 
-// for (var i = 0; i < array.length; i++) {
-//     for (var j = 0; j < i.length; j++) {
-//         console.log(age)
-//     }
-// }
-
 //a) Yashi 18-25 araliginda olan telebelerin sayi.
 
 // function countOfStudents(array) {
 //     let count = 0;
 //     for (let i = 0; i < array.length; i++) {
-//         if (i.age >= 18 && i.age <= 25) {
+//         if (array[i].age >= 18 && array[i].age <= 25) {
 //             count++;
 //         }
 //     }
@@ -151,15 +140,13 @@
 
 ////b) Emailinde "c" herfi olan studentlerin sayi.
 
-// function checkEmail(arrayi){
+// function checkEmail(array){
 //     let count = 0;
-//     for (let i = 0; i < arrayi.length; i++) {
-//         for (let j = 0; j < i.length; j++) {
-//             for (let h = 0; h < j.length; h++) {
-//                 if(j[h] == "c"){
-//                     count++;
-//                 }
-//             }
+//     for (let i = 0; i < array.length; i++) {
+//         let emailArray = array[i].email.split("@");
+//         let trueEmail = emailArray[0];
+//         if(trueEmail.includes("c")){
+//             count++;
 //         }
 //     }
 //     return count;
@@ -172,11 +159,298 @@
 // text1 = "salam"
 
 // function changeFirstLetter(text){
-//     for (let i = 0; i < text.length; i++) {
-//         text[i] == text [i].toUpperCase()
-//         break;
-//     }
-//     return text;
+//     let modifiedText = "";
+//     modifiedText += text[0].toUpperCase();
+//     modifiedText += text.slice(1);
+//     return modifiedText;
 // }
 
-// console.log(changeFirstLetter(text1))
+// console.log(changeFirstLetter(text1));
+
+
+
+
+
+// let person = {
+//     name: "Ismayil",
+//     surname: "Ceferli",
+//     age: 24
+// }
+
+// document.querySelector("h1").innerText = person.name
+
+
+
+// let datas = [
+//     {
+//         name: "Nurlan",
+//         surname: "Umudov",
+//         groups: [
+//             "Pb101",
+//             "Pb102"
+//         ],
+//         teachers: [
+//             "Hesen",
+//             "Elxan"
+//         ]
+//     },
+
+//     {
+//         name: "Kamran",
+//         surname: "Muradov",
+//         groups: [
+//             "Pb101",
+//         ],
+//         teachers: [
+//             "Hesen"
+//         ]
+//     }
+// ]
+
+
+
+// let surname = "Fexriyye Tagizade";
+
+// console.log(surname.length);
+
+// console.log(surname.toLowerCase());
+
+// console.log(surname.toUpperCase());
+
+// console.log(surname.trim());
+
+// console.log(surname.startsWith("T"));
+
+// console.log(surname.indexOf("a"));
+
+// console.log(surname.lastIndexOf("a"));
+
+// console.log(surname.substring(0,3));
+
+// for (let i = 0; i < surname.length; i++) {
+//     const element = surname[i];
+    
+// }
+
+// console.log(surname.includes("t".toUpperCase()));
+
+// let array5 = surname.split(" ");
+
+// console.log(array5[0]);
+
+// console.log(surname.charAt(1));
+
+// console.log(surname.charCodeAt("1"));
+
+// console.log(surname.slice(4));
+
+// console.log(surname.slice(4,surname.length));
+
+// console.log(surname.replace("a", "b"));
+
+// console.log(surname.replaceAll("a", "b"));
+
+
+// let nums = [1,2,3,4,6,8,9];
+
+// for (const i of nums) {
+//     console.log(i);
+// }
+
+// let datas = [
+//     {
+//         name: "Nurlan",
+//         surname: "Umudov",
+//         groups: [
+//             "Pb101",
+//             "Pb102"
+//         ],
+//         teachers: [
+//             "Hesen",
+//             "Elxan"
+//         ]
+//     },
+
+//     {
+//         name: "Kamran",
+//         surname: "Muradov",
+//         groups: [
+//             "Pb101",
+//         ],
+//         teachers: [
+//             "Hesen"
+//         ]
+//     }
+// ]
+
+// for (const key in datas) {
+//     console.log(datas[key]);
+// }
+
+// for (const key in datas) {
+//     console.log(datas[key].name);
+// }
+
+// let nums = [1,2,2,3];
+
+// nums.forEach((element,i) => {
+//     console.log(element + " " + i)
+// });
+
+// let data = nums.toString();
+
+// console.log(data);
+
+// let data1 = nums.join("-")
+
+// nums.pop();
+
+// nums.push(5);
+
+// nums.shift();
+
+// nums.unshift(5);
+
+// console.log(nums);
+
+// const myGirls = ["Cecilie", "Lone"];
+// const myBoys = ["Emil", "Tobias", "Linus"];
+// const myChildren = myGirls.concat(myBoys);
+
+// console.log(myChildren);
+
+// let address = "Ehmedli";
+
+// let result = [...address];
+
+// console.log(result);
+
+
+// let obj = {
+//     name: "Semed",
+//     surname: "Semedov"
+// }
+
+// let obj2 = {...obj};
+
+// console.log(obj2);
+
+
+
+// function showText(){
+//     console.log("Salam");
+// }
+
+// function showText(text){
+//     console.log(text);
+// }
+
+// showText("Salam");
+
+
+// function sum(a, b){
+//     let result = a + b;
+//     console.log(result);
+// }
+
+// sum(5,10);
+
+// let datas1 = [1,2,34,5,76]
+
+// function findData(array){
+//     for (const i of array) {
+//         if(i == 10){
+//             console.log("Found");
+//             return;
+//         }
+//     }
+// }
+
+// findData(array);
+
+
+// function sumOfNums(a, b){
+//     return a + b;
+// }
+
+// console.log(sumOfNums(5, 10));
+
+// let datas1 = [1,2,34,5,76]
+
+// function test(array){
+//     array[0] = 100;
+//     console.log(array);
+// }
+
+// test(datas1);
+
+// let b = 10;
+
+// function test2(a){
+//     a = 20;
+//     console.log(a);
+// }
+
+// test2(b);
+
+// console.log(b);
+
+// let datas1 = [1,2,34,5,76]
+
+// function test3 (...array) {
+//     for (let i = 0; i < array.length; i++) {
+//         const element = array[i];
+//         console.log(element);
+//     }
+// }
+
+// test3(1,2,3);
+
+
+// function test() {
+//     console.log(arguments);
+// }
+
+// test(1,2,3,5);
+
+
+// let showText = () => {
+//     console.log("text");
+// }
+
+// showText();
+
+// let showText = (text) => {
+//     console.log(text);
+// }
+
+// showText("Salam");
+
+// function showNumber() {
+//     console.log(this);
+// }
+
+// showNumber();
+
+// const showNum = () => {
+//     console.log(this)
+// }
+
+// showNum();
+
+
+// let student = {
+//     name: "Reshad",
+//     surname: "Aghayev",
+//     age: 22,
+//     address: "Xalqlar Dostlugu",
+//     getFullName: function(){
+//         console.log(this.name + " " + this.surname);
+//     },
+//     getFullData: () => {
+//         console.log(this);
+//     }
+// }
+
+// student.getFullName();
+// student.getFullData();
